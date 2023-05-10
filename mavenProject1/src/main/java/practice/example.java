37067e081321a7ce0582d2bc41797e1e39b79243
91889 <91889@LAPTOP-P55RKB0V>
2023-05-10 20:13:37 +0530

    first program

---
+++ mavenProject1/src/main/java/practice/example.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class example {
+ void method() {
+	 System.out.println("method");
+ }
+ public static void main(String[] args) {
+	 example e=new example();
+	 e.method();
+ }
+}
